56a8b0bd61333a01346cd64d6ee087e70f51ef1d
Moemen47 <moemenjebali4720@gmail.com>
2022-01-04 22:36:45 +0100

    m commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Profile from './component/Profile'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+class App extends Component {
+    
+   state = {
+imgSrc: '/m.JPG',
+fullName: 'Moemen Jebali',
+bio: 'Customer Service Representative ',
+Profession: 'Customer Service Representative',
+show: false,
+   };
+   render(){
+     const { show } = this.state;
+     const showMe = show ? ('cacher profil') : ('afficher profil');
+     return(
+       <div className='text-center'>
+         <button className='btn btn-danger mt-5' onClick={() => this.setState ({show: !show})}>{ showMe }</button>
+         {show && <Profile info={this.state} />}
+       </div>
+     )
+   }
+  }
+  export default App
---
+++ src/component/Profile.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+class Profile extends Component {
+    state = {
+        timer: 0,
+}
+componentDidMount(){
+    setInterval(()=> {
+        this.setState({
+            timer: this.state.timer +1
+        })
+    },1000);   
+}
+componentWillUnmount(){
+    clearInterval(this.Interval)
+}
+render (){
+    return (
+        <div className='container card mt-5' style={{width: '25%'}}>
+            <button className='btn btn-primary mt-5'>{'has passed ${this.state.timer} to enter page'} </button>
+          <div className='text-center'>
+              <div className='card header mb-5'>
+                  <img src={ this.props.info.imgSrc } alt='logo' style={{maxWidth: '100%' }}/>
+              </div>
+              <ul className='list-group'>
+                  <li className='list-group-item'>{this.props.info.fullName}</li>
+                  <li className='list-group-item'>{this.props.info.bio}</li>
+                  <li className='list-group-item'>{this.props.info.profession}</li>
+              </ul>
+              </div>  
+        </div>
+    )
+}
+}
+export default App
